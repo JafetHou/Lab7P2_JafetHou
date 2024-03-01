a18e4fe1cf216f5685a606ed43e12a42c46fa07f
JafetHou <jafethou2005@gmail.com>
2024-03-01 13:55:00 -0600

    Interfaz grafica

---
+++ src/Lab7P2_JafetHou/Inventario.java
@@ -0,0 +1,12 @@
+
+package Lab7P2_JafetHou;
+
+
+public class Inventario {
+    
+    private int id, categoria;
+    private String nombre, aisle;
+    private float price;
+    private int bin;
+    
+}
